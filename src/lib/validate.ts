10892c400372e019da1c0edc90e906aa8f3ccf25
Jay Baker <logikal@gmail.com>
2023-04-28 13:13:32 +1200

    feat: validate files don't already exist

---
+++ src/lib/validate.ts
@@ -0,0 +1,15 @@
+import fs from "node:fs"
+import { doReplacements } from "./replacements"
+import { getVariables } from "./variables"
+
+export function validateFilesDontExist(files: ScaffoldFile[], input: Record<string, any>) {
+  for (let file of files) {
+    let fileName = doReplacements(file.name, input, getVariables(file.name))
+    if (fs.existsSync(fileName)) {
+      console.log('')
+      console.log(`File already exists at ${fileName}.`)
+      console.log(` ❌ Aborted.`)
+      process.exit(1)
+    }
+  }
+}
